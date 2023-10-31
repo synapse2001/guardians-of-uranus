3b8784533f4f349cd6c6582d870456770b9dd142
Gagan Agarwal <73771646+synapse2001@users.noreply.github.com>
2023-10-31 15:57:42 +0530

    added teaser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import React from 'react';
 import logo from './logo.png';
-import gif_logo from './gif_meme_team2.gif';
+import YouTube from 'react-youtube';
 import './App.css';
 
 function App() {
+  const containerStyle = {
+    marginTop: '10px',
+    width: '100%',
+    maxWidth: '600px', // Adjust the maximum width as needed
+  };
+
+  const logoStyle = {
+    width: '50px',
+    height: 'auto',
+    marginRight: '10px',
+  };
+
+  const opts = {
+    width: '100%',
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-      <p>Check Back Later !</p>
-      {/* <img src = {logo} width="300" height="300"></img> */}
-      <img src = {gif_logo} width="100%" ></img>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={logo} alt="Static Logo" style={logoStyle} />
+          <h1 style={{ textAlign: 'center', fontSize: '20px' }}>Guardians of Uranus</h1>
+        </div>
+        <div style={containerStyle}>
+          <YouTube videoId="AejnPiw30lw" opts={opts} />
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
